8e1aa49620d9caa8df36b7e762b69179dd31e7f9
saif <saif@tradexa.co.in>
2026-04-19 23:10:26 +0530

    tasks loaded

--- src/app/core/services/task.service.ts
+++ src/app/core/services/task.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Task } from '../models/task.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:3000/tasks';
+
+  constructor(private http: HttpClient) { }
+
+  getTasks() {
+    return this.http.get<Task[]>(this.baseUrl);
+  }
+
+  getTaskById(id: string) {
+    return this.http.get<Task>(`${this.baseUrl}/${id}`);
+  }
+
+  addTask(task: Task) {
+    return this.http.post(this.baseUrl, task);
+  }
+
+  updateTask(id: string, task: Task) {
+    return this.http.put(this.baseUrl, task);
+  }
+
+  deleteTask(id: string) {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
 }
--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../core/services/task.service';
+import { RouterLink } from '@angular/router';
+import { Task } from '../../core/models/task.model';
+
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
 
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+
+  ngOnInit() {
+    this.loadTasks();
+  }
+
+
+  loadTasks() {
+    this.taskService.getTasks().subscribe((data) => {
+      this.tasks = data
+    })
+  }
+
+
+  deleteTask(id: string) {
+    if (confirm('Delete this tasks?')) {
+      this.taskService.deleteTask(id).subscribe(() => {
+        this.loadTasks();
+      })
+    }
+  }
 }
