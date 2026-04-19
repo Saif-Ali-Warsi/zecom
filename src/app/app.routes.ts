026c3111487ec6a38387d37a4c4c4129d02c8bdc
saif <saif@tradexa.co.in>
2026-04-19 22:00:33 +0530

    task manager setup done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: 'tasks',
+        loadComponent: () => import('./tasks/task-list/task-list.component').then(m => m.TaskListComponent)
+    },
+    {
+        path: 'task/add',
+        loadComponent: () => import('./tasks/task-form/task-form.component').then(m => m.TaskFormComponent)
+    },
+    {
+        path: 'task/edit/:id',
+        loadComponent: () => import('./tasks/task-form/task-form.component').then(m => m.TaskFormComponent)
+    }
+];
